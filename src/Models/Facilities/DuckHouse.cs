731418388a724213fc4ef8ff9a4d72a9e51618d2
DanielBradleyBennett <danielbradleybennett@gmail.com>
2020-03-16 14:27:27 -0500

    pushing up animal count

--- src/Models/Facilities/DuckHouse.cs
+++ src/Models/Facilities/DuckHouse.cs
@@ -6,45 +6,45 @@ using Trestlebridge.Interfaces;
 
 namespace Trestlebridge.Models.Facilities
 {
-    public class DuckHouse : IFacility<IDuckHouse>
+  public class DuckHouse : IFacility<IDuckHouse>
+  {
+    private int _capacity = 5;
+    private Guid _id = Guid.NewGuid();
+
+    private List<IDuckHouse> _animals = new List<IDuckHouse>();
+
+    public double Capacity
     {
-        private int _capacity = 50;
-        private Guid _id = Guid.NewGuid();
-
-        private List<IDuckHouse> _animals = new List<IDuckHouse>();
-
-        public double Capacity
-        {
-            get
-            {
-                return _capacity;
-            }
-        }
-
-        public void AddResource(IDuckHouse animal)
-        {
-            // TODO: implement this...
-            _animals.Add(animal);
-            // throw new NotImplementedException ();
-        }
-
-        public void AddResource(List<IDuckHouse> animals)
-        {
-
-            // TODO: implement this...
-
-            // throw new NotImplementedException ();
-        }
-
-        public override string ToString()
-        {
-            StringBuilder output = new StringBuilder();
-            string shortId = $"{this._id.ToString().Substring(this._id.ToString().Length - 6)}";
-
-            output.Append($"Duck House {shortId} has {this._animals.Count} animals\n");
-            this._animals.ForEach(a => output.Append($"   {a}\n"));
-
-            return output.ToString();
-        }
+      get
+      {
+        return _capacity;
+      }
     }
+
+    public void AddResource(IDuckHouse animal)
+    {
+      // TODO: implement this...
+      _animals.Add(animal);
+      // throw new NotImplementedException ();
+    }
+
+    public void AddResource(List<IDuckHouse> animals)
+    {
+
+      // TODO: implement this...
+
+      // throw new NotImplementedException ();
+    }
+
+    public override string ToString()
+    {
+      StringBuilder output = new StringBuilder();
+      string shortId = $"{this._id.ToString().Substring(this._id.ToString().Length - 6)}";
+
+      output.Append($"Duck House {shortId} has {this._animals.Count} animals\n and {this._capacity - this._animals.Count} spaces left.");
+      this._animals.ForEach(a => output.Append($"   {a}\n"));
+
+      return output.ToString();
+    }
+  }
 }
